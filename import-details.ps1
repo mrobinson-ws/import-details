fb151688ce4d729755c15bf664ee0f8651f64320
Mike Robinson <psikotick@gmail.com>
2021-06-05 00:04:00 -0400

    Created Script

--- import-details.ps1
+++ import-details.ps1
@@ -1,1 +1,9 @@
+#Require -Modules AzureAD, ImportExcel
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$ExcelDoc
+)
 
+$users = Import-Excel -Path $ExcelDoc
+
+foreach ($user in $users){Set-AzureADUser -ObjectId $user.Email -CustomAtrribute1 $user.Type -City $user.City -State $user.State}
