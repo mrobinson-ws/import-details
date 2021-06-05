8379ccb61e95122f00a35e747d288736686456a7
Mike Robinson <psikotick@gmail.com>
2021-06-05 07:02:17 -0400

    Updated and Working

--- import-details.ps1
+++ import-details.ps1
@@ -1,6 +1,24 @@
-Connect-AzureAD
-Connect-ExchangeOnline
+# Test And Connect To AzureAD If Needed
+try {
+    Write-Verbose -Message "Testing connection to Azure AD"
+    Get-AzureAdDomain -ErrorAction Stop | Out-Null
+    Write-Verbose -Message "Already connected to Azure AD"
+}
+catch {
+    Write-Verbose -Message "Connecting to Azure AD"
+    Connect-AzureAD
+}
 
+#Test And Connect To Microsoft Exchange Online If Needed
+try {
+    Write-Verbose -Message "Testing connection to Microsoft Exchange Online"
+    Get-Mailbox -ErrorAction Stop | Out-Null
+    Write-Verbose -Message "Already connected to Microsoft Exchange Online"
+}
+catch {
+    Write-Verbose -Message "Connecting to Microsoft Exchange Online"
+    Connect-ExchangeOnline
+}
 Param(
     [Parameter(Mandatory=$true)]
     [string]$ExcelDoc
